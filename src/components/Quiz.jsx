45d7bac60f11fb66aeca41a0b1a10a57403a13f5
jordin <jordinmathew.mec@gmail.com>
2024-10-30 21:24:21 +1000

    shuffled quiz answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,28 +1,42 @@
 import { useState } from "react";
-import QUESTIONS from '../questions';
-export default function Quiz(){
-  const [userAnswers,setUserAnswers] = useState([]);
+import QUESTIONS from "../questions";
+import quizCompleteImage from "../assets/quiz-complete.png";
+export default function Quiz() {
+  const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectAnswer(selectedAnswer){
-    setUserAnswers((prev)=>{
-      return [...prev,selectedAnswer];
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prev) => {
+      return [...prev, selectedAnswer];
     });
   }
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImage} alt="Trophy image" />
+        <h2>SUMMARY</h2>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
-  return(
-    <div id='quiz'>
-    <div id="question">
-  <h2 >{QUESTIONS[activeQuestionIndex].text}</h2>
-  <ul id="answers">
-    {QUESTIONS[activeQuestionIndex].answers.map((answer)=>(
-      <li key={answer} className="answer">
-        <button onClick={()=>handleSelectAnswer(answer)}>{answer}</button>
-      </li>
-    ))}
-  </ul>
-  </div>
-  </div>
+  return (
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
-
 }
